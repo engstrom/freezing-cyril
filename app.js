3af6211cbad03717624f8f11caa3f28b709abfba
engstrom <dengstr@wgu.edu>
2014-04-13 09:54:52 -0600

    changed from decimal to int

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var app = angular.module("progressApp", []);
 
 app.controller("ProgressController", function ($scope, $http) {
-  $http.get('https://rawgit.com/engstrom/freezing-cyril/master/progress.json').success(function(result) {
+  $http.get('https://rawgit.com/engstrom/freezing-cyril/master/progress.json')
+    .success(function(result) {
       $scope.courses = result;
     });
 });
-
-// Add percentage filter
-app.filter('percentage', ['$filter', function($filter) {
-    return function(input, decimals) {
-        return $filter('number')(input*100, decimals)+'%';
-    };
-}]);
