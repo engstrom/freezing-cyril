c9bf5a69c4833d9440ed3e0d82a0894ef44a9874
engstrom <dengstr@wgu.edu>
2014-04-13 10:13:17 -0600

    changed json location

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("progressApp", []);
 
 app.controller("ProgressController", function ($scope, $http) {
-  $http.get('https://rawgit.com/engstrom/freezing-cyril/master/progress.json')
+  $http.get('https://rawgit.com/engstrom/school-progress/master/data/progress.json')
     .success(function(result) {
       $scope.courses = result;
     });
