387097ac227ee481d1de22220f69c1586ef8517b
engstrom <dengstr@wgu.edu>
2014-04-27 14:01:22 -0600

    new json path

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("progressApp", []);
 
 app.controller("ProgressController", function ($scope, $http) {
-  $http.get('https://rawgit.com/engstrom/school-progress/master/data/progress.json')
+  $http.get('data/progress.json')
     .success(function(result) {
       $scope.courses = result;
     });
